802eafeb51afddb77c6194e00a5246316bf21e21
Edigleison Alves <Edigleison Alves>
2016-09-16 00:33:29 -0300

    Developing functionality

---
+++ module1-solution/app.js
@@ -0,0 +1,14 @@
+(function () {
+  angular.module('lunchApp', [])
+  .controller('LunchCtrl', function($scope){
+      $scope.countItems = function(){
+        var count = $scope.items.split(",").length();
+        if(count <= 3){
+            $scope.message = "Menor igual a 3";
+        }else{
+            $scope.message = "Maior que 3";
+        }
+
+      }
+  });
+)();
