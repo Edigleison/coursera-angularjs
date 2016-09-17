58bf6e5fc713e7e30d672c2d0a202fd8448421e2
Edigleison Alves <Edigleison Alves>
2016-09-17 01:11:53 -0300

    Developing functionality

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -2,32 +2,37 @@
   'use strict';
 
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController', ['$scope', function($scope){
-      $scope.countItems = function(){
-        var count = 0;
-        if($scope.items != undefined){
+  .controller('LunchCheckController', ctrl);
+	ctrl.$inject = ['$scope'];
+	function ctrl ($scope){
+    $scope.countItems = function(){
+      var count = 0;
+      if($scope.items != undefined){
 
-          var list = $scope.items.split(",");
-          var i;
-          for(i in list){
-            if(list[i].trim().length > 0){
-              count++;
-            }
+        var list = $scope.items.split(",");
+        var i;
+        for(i in list){
+          if(list[i].trim().length > 0){
+            count++;
           }
         }
-
-        if(count == 0){
-          $scope.message = "Please enter data first";
-        }else if(count <= 3){
-            $scope.message = "Enjoy!";
-        }else{
-            $scope.message = "Too much!";
-        }
-
-      };
-
-      $scope.isValid = function(){
-        return $scope.message === "Enjoy!" || $scope.message === "Too much!";
       }
-  }]);
+
+      if(count == 0){
+        $scope.messageClass = "alert-danger";
+        $scope.message = "Please enter data first";
+      }else if(count <= 3){
+          $scope.messageClass = "alert-success";
+          $scope.message = "Enjoy!";
+      }else{
+          $scope.messageClass = "alert-success";
+          $scope.message = "Too much!";
+      }
+
+    };
+
+    $scope.isValid = function(){
+      return $scope.message === "Enjoy!" || $scope.message === "Too much!";
+    }
+  };
 })();
