6327af41240700f9748386e1d442781c84caa4ca
Edigleison Alves <Edigleison Alves>
2016-09-17 00:42:13 -0300

    development of assingnment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,14 +1,33 @@
 (function () {
-  angular.module('lunchApp', [])
-  .controller('LunchCtrl', function($scope){
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', ['$scope', function($scope){
       $scope.countItems = function(){
-        var count = $scope.items.split(",").length;
-        if(count <= 3){
-            $scope.message = "Menor igual a 3";
+        var count = 0;
+        if($scope.items != undefined){
+
+          var list = $scope.items.split(",");
+          var i;
+          for(i in list){
+            if(list[i].trim().length > 0){
+              count++;
+            }
+          }
+        }
+
+        if(count == 0){
+          $scope.message = "Please enter data first";
+        }else if(count <= 3){
+            $scope.message = "Enjoy!";
         }else{
-            $scope.message = "Maior que 3";
+            $scope.message = "Too much!";
         }
 
       };
-  });
+
+      $scope.isValid = function(){
+        return $scope.message === "Enjoy!" || $scope.message === "Too much!";
+      }
+  }]);
 })();
